9ebed5f706380601b73644d22ef8f9aa8dfbcfa6
Rohit Thakre <thake785@gmail.com>
2024-05-04 23:10:42 +0530

    comments removed

--- account/admin.py
+++ account/admin.py
@@ -1,34 +1,3 @@
-
-# from django.contrib import admin
-# from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-# from account import models
-
-
-
-# class UserAdmin(BaseUserAdmin):
- 
-#     list_display = ["email", "name", "is_admin"]
-#     list_filter = ["is_admin","name"]
-
-#     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
-#     # overrides get_fieldsets to use this attribute when creating a user.
-#     add_fieldsets = [
-#         (
-#             None,
-#             {
-#                 "classes": ["wide"],
-#                 "fields": ["email", "name", "password1", "password2","is_active","is_admin"],
-#             },
-#         ),
-#     ]
-#     search_fields = ["email","name"]
-#     ordering = ["email"]
-#     filter_horizontal = []
-
-
-# admin.site.register(models.CustomUser, UserAdmin)
-
-
 from django.contrib import admin
 from account import models
 
